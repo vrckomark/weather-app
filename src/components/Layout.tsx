5332c3b90874907a53c0cf75065903f27c4344bf
vrckomark <vrckomark@gmail.com>
2022-10-14 18:34:34 +0200

    made responsive

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,15 +4,7 @@ import WMO from "../WMO.module";
 export default function Layout(weatherData: any) {
   const curWeather = weatherData.current_weather;
 
-  const weekDays = [
-    "Nedelja",
-    "Ponedeljek",
-    "Torek",
-    "Sreda",
-    "Četrtek",
-    "Petek",
-    "Sobota",
-  ];
+  const weekDays = ["Ned", "Pon", "Tor", "Sre", "Čet", "Pet", "Sob"];
 
   const temp: any = weatherData.daily.time.slice(1);
   const dailyTimes: any = [];
@@ -55,12 +47,14 @@ export default function Layout(weatherData: any) {
   );
   return (
     <>
-      <main className="text-2xl text-white">
+      <main className="text-2xl text-white sm:text-3xl lg:text-4xl 2xl:text-5xl">
         <div className="flex flex-col justify-center items-center px-2">
           <div className="grid grid-cols-8 grid-rows-2 w-full bg-white bg-opacity-20 rounded-xl mb-8 mt-8">
             <div className="col-span-8 flex flex-col justify-between px-6 pb-6 items-center">
-              <div className="flex items-center justify-between w-full px-4">
-                <div className="text-6xl">{curWeather.temperature}°C</div>
+              <div className="flex items-center justify-between w-full">
+                <div className="text-6xl lg:text-8xl 2xl:text-9xl">
+                  {curWeather.temperature}°C
+                </div>
                 <>
                   {WMO.map((obj) => {
                     return (
@@ -69,7 +63,7 @@ export default function Layout(weatherData: any) {
                           <img
                             src={obj.svg}
                             alt={obj.slug}
-                            className="w-24 mr-10"
+                            className="w-1/4 mr-8"
                           />
                         ) : null}
                       </>
@@ -77,12 +71,14 @@ export default function Layout(weatherData: any) {
                   })}
                 </>
               </div>
-              <div className="w-full flex pl-8">Čuti se kot {feelsLike}°C</div>
+              <div className="w-full flex pl-2 2xl:mb-14 2xl:ml-8">
+                Čuti se kot {feelsLike}°C
+              </div>
             </div>
             {weatherCodes.map((obj, index) => {
               return (
                 <div>
-                  <div className="text-base flex justify-center">
+                  <div className="text-base flex justify-center sm:text-lg lg:text-2xl 2xl:text-4xl">
                     {(times[index].getHours() + 1).toString().length < 2 ? (
                       <>0</>
                     ) : null}
@@ -97,7 +93,7 @@ export default function Layout(weatherData: any) {
                       />
                     ) : null;
                   })}
-                  <div className="text-lg  flex justify-center">
+                  <div className="text-lg  flex justify-center sm:text-xl lg:text-3xl 2xl:text-5xl">
                     {temps[index]}°C
                   </div>
                 </div>
@@ -107,16 +103,30 @@ export default function Layout(weatherData: any) {
           <div className="flex flex-col w-full">
             {dailyTimes.map((time: Date, i: number) => {
               return (
-                <div className="bg-white bg-opacity-20 my-2 grid grid-cols-4 rounded-xl items-center px-2 text-xl">
-                  <div>{weekDays[time.getDay()]}</div>
-                  <div>{dailyTempMin[i]}°C</div>
-                  <div>{dailyTempMax[i]}°C</div>
+                <div className="bg-white bg-opacity-20 my-2 grid grid-cols-4 rounded-xl items-center px-2 text-base py-2 sm:text-xl lg:text-2xl 2xl:text-4xl 2xl:py-0">
+                  <div className="pl-4 2xl:pl-8">{weekDays[time.getDay()]}</div>
+                  {i === 0 ? (
+                    <>
+                      <div className="after:content-['Min'] after:text-xs after:text-gray-200 after:absolute after:-translate-x-6 after:-translate-y-6 lg:after:text-base lg:after:-translate-x-8 2xl:after:text-lg 2xl:after:-translate-x-10">
+                        {dailyTempMin[i]}°C
+                      </div>
+                      <div className="after:content-['Max'] after:text-xs after:text-gray-200 after:absolute after:-translate-x-10 after:-translate-y-6 lg:after:text-base lg:after:-translate-x-12 2xl:after:text-lg 2xl:after:-translate-x-16">
+                        {dailyTempMax[i]}°C
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div>{dailyTempMin[i]}°C</div>
+                      <div>{dailyTempMax[i]}°C</div>
+                    </>
+                  )}
+
                   {WMO.map((item) => {
                     return (
                       <>
                         {item.weathercode === dailyWeatherCodes[i] ? (
                           <img
-                            className="w-16"
+                            className="w-14 sm:w-16 lg:w-20 2xl:w-28"
                             src={item.svg}
                             alt={item.slug}
                           />
