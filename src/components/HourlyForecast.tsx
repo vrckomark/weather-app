1100b81297459522e43bfbb9175247f744a4ee79
vrckomark <vrckomark@gmail.com>
2022-11-04 16:06:21 +0100

    added chart for temperatures, still in progress

--- src/components/HourlyForecast.tsx
+++ src/components/HourlyForecast.tsx
@@ -1,7 +1,7 @@
 import WMO from "../WMO.module";
 import forecastType from "../types/ForecastType";
 import WMOType from "../types/WMOType";
-import hourlyForecastType from "../types/HourlyForecastType";
+import { timeStamp } from "console";
 
 interface hourlyObj {
   time: Date;
@@ -10,59 +10,14 @@ interface hourlyObj {
   apparent_temperature: number;
 }
 
-const HourlyForecast = (forecastData: forecastType) => {
+const HourlyForecast = ({ hourlyForecast }: any) => {
   const isNight = (obj: hourlyObj) => {
     return obj.time.getHours() > 18 || obj.time.getHours() < 5;
   };
 
-  var timesUnix: any = [];
-
-  forecastData.hourly.time.map((time: any) => {
-    return timesUnix.push(Date.parse(time));
-  });
-
-  const now = timesUnix.indexOf(
-    timesUnix.reduce((prev: any, curr: any) =>
-      Math.abs(curr - Date.now()) < Math.abs(prev - Date.now()) ? curr : prev
-    )
-  );
-
-  var obj: any, svg: any;
-  var hourlyForecast: any = [];
-  timesUnix.forEach((time: any, i: number) => {
-    if (i >= now - 1 && i <= now + 7) {
-      time = new Date(time);
-      WMO.map((item: any) => {
-        return item.weathercode ===
-          forecastData.hourly.weathercode[
-            i + forecastData.utc_offset_seconds / 60 / 60
-          ]
-          ? (item.hasNight && time.getHours() > 18) || time.getHours() < 5
-            ? (svg = item.svgNight)
-            : (svg = item.svg)
-          : null;
-      });
-      obj = {
-        weathercode:
-          forecastData?.hourly.weathercode[
-            i + forecastData.utc_offset_seconds / 60 / 60
-          ],
-        time: new Date(forecastData.hourly.time[i]),
-        temperature_2m:
-          forecastData.hourly.temperature_2m[
-            i + forecastData.utc_offset_seconds / 60 / 60
-          ],
-        svg,
-      };
-      hourlyForecast.push(obj);
-    }
-  });
-
-  hourlyForecast = hourlyForecast.slice(1);
-
   return hourlyForecast.map((obj: hourlyObj, i: number) => {
     return obj.time.getHours() > 18 || obj.time.getHours() < 5 ? (
-      <div className="bg-black bg-opacity-30 flex flex-col justify-center items-center last:rounded-r-xl first:rounded-l-xl">
+      <div className="bg-black bg-opacity-30  pt-6 pb-2 flex flex-col justify-center items-center last:rounded-r-xl first:rounded-l-xl">
         <div className="text-base flex justify-center sm:text-lg lg:text-2xl 2xl:text-2xl ">
           {(obj.time.getHours() + 1).toString().length < 2 ? <>0</> : null}
           {obj.time.getHours() === 23 ? (
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,53 @@
-import { useEffect, useMemo } from "react";
+import { useMemo } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale,
+  Tooltip,
+} from "chart.js";
+
 import WMO from "../WMO.module";
 import HourlyForecast from "./HourlyForecast";
 import DailyForecast from "./DailyForecast";
 import forecastType from "../types/ForecastType";
 import Footer from "./Footer";
 
-export default function Layout(weatherData: forecastType) {
-  const weekDaysShort = ["Ned", "Pon", "Tor", "Sre", "Čet", "Pet", "Sob"];
-  const weekDays = [
-    "Nedelja",
-    "Ponedeljek",
-    "Torek",
-    "Sreda",
-    "Četrtek",
-    "Petek",
-    "Sobota",
-  ];
+ChartJS.register(
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale,
+  Tooltip
+);
 
+const weekDaysShort = ["Ned", "Pon", "Tor", "Sre", "Čet", "Pet", "Sob"];
+const weekDays = [
+  "Nedelja",
+  "Ponedeljek",
+  "Torek",
+  "Sreda",
+  "Četrtek",
+  "Petek",
+  "Sobota",
+];
+
+const currentTime: Date = new Date(Date.now());
+const isNight: boolean =
+  currentTime.getHours() >= 19 || currentTime.getHours() < 5;
+
+const getTime = (time: Date) => {
+  let _temp: string = "";
+  (time.getHours() + 1).toString().length < 2 ? (_temp += "0") : (_temp += "");
+  time.getHours() === 23 ? (_temp += "00") : (_temp += time.getHours());
+  return _temp + ":00";
+};
+
+export default function Layout(weatherData: forecastType) {
   const weatherDataProp = { ...weatherData };
 
   const getNow = useMemo(() => {
@@ -34,16 +65,56 @@ export default function Layout(weatherData: forecastType) {
     return now;
   }, [weatherData]);
 
-  const currentTime = new Date(Date.now());
-  const isNight: boolean =
-    currentTime.getHours() >= 19 || currentTime.getHours() < 5;
+  var timesUnix: any = [];
+
+  weatherData.hourly.time.map((time: any) => {
+    return timesUnix.push(Date.parse(time));
+  });
+
+  const now = timesUnix.indexOf(
+    timesUnix.reduce((prev: any, curr: any) =>
+      Math.abs(curr - Date.now()) < Math.abs(prev - Date.now()) ? curr : prev
+    )
+  );
+
+  var obj: any, svg: any;
+  var hourlyForecast: any = [];
+  timesUnix.forEach((time: any, i: number) => {
+    if (i >= now - 1 && i <= now + 7) {
+      time = new Date(time);
+      WMO.map((item: any) => {
+        return item.weathercode ===
+          weatherData.hourly.weathercode[
+            i + weatherData.utc_offset_seconds / 60 / 60
+          ]
+          ? (item.hasNight && time.getHours() > 18) || time.getHours() < 5
+            ? (svg = item.svgNight)
+            : (svg = item.svg)
+          : null;
+      });
+      obj = {
+        weathercode:
+          weatherData?.hourly.weathercode[
+            i + weatherData.utc_offset_seconds / 60 / 60
+          ],
+        time: new Date(weatherData.hourly.time[i]),
+        temperature_2m:
+          weatherData.hourly.temperature_2m[
+            i + weatherData.utc_offset_seconds / 60 / 60
+          ],
+        svg,
+      };
+      hourlyForecast.push(obj);
+    }
+  });
+  hourlyForecast = hourlyForecast.slice(1);
 
   return (
     weatherData && (
       <>
         <main className="mt-8 text-2xl text-white sm:text-3xl sm:mx-8 md:mx-14 lg:text-4xl lg:mx-24 xl:mx-64 2xl:text-5xl 2xl:mx-80">
           <div className="flex flex-col justify-center items-center px-2">
-            <div className="grid grid-cols-8 grid-rows-2 w-full bg-white bg-opacity-20 rounded-xl mb-8">
+            <div className="grid grid-cols-8 w-full bg-white bg-opacity-20 rounded-xl mb-8">
               <div className="col-span-8 flex flex-col justify-between px-6 pb-6 items-center 2xl:px-12">
                 <div className="flex items-center justify-between w-full ">
                   <div className="text-6xl lg:text-7xl 2xl:text-8xl">
@@ -68,7 +139,81 @@ export default function Layout(weatherData: forecastType) {
                   °C
                 </div>
               </div>
-              <HourlyForecast {...weatherDataProp} />
+              <HourlyForecast hourlyForecast={hourlyForecast} />
+              <Line
+                className="mb-4 px-6"
+                options={{
+                  scales: {
+                    x: {
+                      grid: {
+                        display: false,
+                        drawTicks: false,
+                        borderWidth: 0,
+                      },
+                      ticks: {
+                        color: "white",
+                        font: {
+                          size: 16,
+                        },
+                      },
+                    },
+                    y: {
+                      grid: {
+                        display: false,
+                        drawTicks: false,
+                        borderWidth: 0,
+                      },
+                      ticks: {
+                        color: "rgba(0,0,0,0)",
+                      },
+                    },
+                  },
+                  responsive: true,
+                  plugins: {
+                    tooltip: {
+                      backgroundColor: "rgba(0, 0, 0,0.1)",
+                      bodyColor: "white",
+
+                      titleSpacing: 4,
+                      titleFont: {
+                        size: 16,
+                      },
+                      bodyFont: {
+                        size: 22,
+                      },
+                      displayColors: false,
+                      padding: 16,
+                      titleAlign: "center",
+                      bodyAlign: "center",
+                      callbacks: {
+                        label: function (context) {
+                          return context.formattedValue + "°C";
+                        },
+                      },
+                    },
+                  },
+                }}
+                data={{
+                  labels: hourlyForecast.map((obj: any) => {
+                    return getTime(obj.time);
+                  }),
+                  datasets: [
+                    {
+                      data: hourlyForecast.map((obj: any) => {
+                        return obj.temperature_2m;
+                      }),
+                      borderColor: "white",
+                      backgroundColor: "white",
+                      cubicInterpolationMode: "monotone",
+                      borderWidth: 3,
+                      pointRadius: 2,
+                      hoverBackgroundColor: "white",
+                      pointHitRadius: 15,
+                    },
+                  ],
+                }}
+                height={40}
+              />
             </div>
             <div className="flex flex-col w-full">
               <DailyForecast
